23b3b07ee63e338881ab478404ed6e707ff7c908
Afonso Machado <afonsosmachado@gmail.com>
2023-07-29 16:08:39 -0300

    feat: :sparkles: validate user id

--- src/main/java/br/com/afonso/resource/PurchaseResource.java
+++ src/main/java/br/com/afonso/resource/PurchaseResource.java
@@ -1,7 +1,7 @@
 package br.com.afonso.resource;
 
 import br.com.afonso.model.Purchase;
-import br.com.afonso.repository.PurchaseRepository;
+import br.com.afonso.service.PurchaseService;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -9,19 +9,19 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/purchases")
 public class PurchaseResource {
 
     @Inject
-    PurchaseRepository purchaseRepository;
+    PurchaseService purchaseService;
 
     @POST
     @Transactional
     @RolesAllowed("user")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void create(Purchase purchase) {
-        purchase.setStatus("ENVIADA");
-        purchaseRepository.persist(purchase);
+    public void create(SecurityContext securityContext, Purchase purchase) {
+        purchaseService.create(securityContext, purchase);
     }
 }
---
+++ src/main/java/br/com/afonso/service/PurchaseService.java
@@ -0,0 +1,29 @@
+package br.com.afonso.service;
+
+import br.com.afonso.model.Purchase;
+import br.com.afonso.model.User;
+import br.com.afonso.repository.PurchaseRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.SecurityContext;
+
+import java.util.Optional;
+
+@ApplicationScoped
+public class PurchaseService {
+
+    @Inject
+    PurchaseRepository purchaseRepository;
+
+    public void create(SecurityContext securityContext, Purchase purchase) {
+        Optional<User> userOptional = User.findByIdOptional(purchase.getUserId());
+        User user = userOptional.orElseThrow();
+
+        if (!user.getUsername().equals(securityContext.getUserPrincipal().getName())) {
+            throw new RuntimeException("O Usuário logado é diferente do userID");
+        }
+
+        purchase.setStatus("ENVIADA");
+        purchaseRepository.persist(purchase);
+    }
+}
