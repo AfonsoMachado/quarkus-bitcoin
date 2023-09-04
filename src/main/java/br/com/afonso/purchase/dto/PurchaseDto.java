b4f1c07c9e5768986b2c15b77ecadec96330487b
Afonso Machado <afonsosmachado@gmail.com>
2023-09-04 15:27:13 -0300

    feat: :sparkles: add swagger

--- src/main/java/br/com/afonso/purchase/dto/PurchaseDto.java
+++ src/main/java/br/com/afonso/purchase/dto/PurchaseDto.java
@@ -1,4 +1,24 @@
 package br.com.afonso.purchase.dto;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PurchaseDto {
+
+    @NotNull
+    private Double price;
+
+    @NotNull
+    private String type;
+
+    @NotNull
+    private String status;
+
 }
